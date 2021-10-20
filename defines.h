461b0872d2ea5ac552458c07bc5fdde9d3767b26
devskale <dev@skale.io>
2021-10-20 13:05:01 +0200

    changed uart 2 / 3 for 446 target

--- defines.h
+++ defines.h
@@ -1,6 +1,5 @@
 
 
-
 #ifdef TARGET_NUCLEO_F466RE
 // I2C Definitions
 //    AS5600      as5600(PB_9, PB_8);
@@ -12,19 +11,14 @@
 #define UART1_TX PA_9
 #define UART1_RX PA_10
 
-#define UART2_TX PA_2
-#define UART2_RX PA_3
+#define UART2_TX PC_10
+#define UART2_RX PC_11
 
-#define UART3_TX PC_10
-#define UART3_RX PC_11
+#define UART3_TX PA_2
+#define UART3_RX PA_3
 
-#define PWM_IN  PC_9
+#define PWM_IN PC_9
 #endif
-
-
-
-
-
 
 #ifdef TARGET_NUCLEO_L432KC
 #define I2C1_SDA PA_10
@@ -35,14 +29,14 @@
 #define UART1_RX PB_7
 
 #define UART2_TX PA_2  //PA_2
-#define UART2_RX PA_15   //PA_3
+#define UART2_RX PA_15 //PA_3
 
-#define PWM_IN  PA_7
+#define PWM_IN PA_7
 
 #endif
 
 // VESC related definitions
-#define VESC_START_BYTE_SMALL     0x2
-#define VESC_START_BYTE_BIG       0x3
-#define VESC_END_BYTE             0x3
-#define VESC_MAX_LENGTH           512
+#define VESC_START_BYTE_SMALL 0x2
+#define VESC_START_BYTE_BIG 0x3
+#define VESC_END_BYTE 0x3
+#define VESC_MAX_LENGTH 512
